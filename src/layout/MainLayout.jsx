cb30b9df3b8f0a969730c86d337d23f523a96808
OLUWASINA OkikiJesu <74295767+okikijesutech@users.noreply.github.com>
2024-06-23 14:34:37 +0100

    Hot fix: page scroll up

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../src/components/Navbar";
 import Footer from "../../src/components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
